11e6f8e9d41341d29bd145bacce9e1c56bbe5d0c
meronmks <meronmks.8914@gmail.com>
2017-11-24 18:35:35 +0900

    LinuxBrewに変更

--- setup/install.sh
+++ setup/install.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-#chmod +x ~/dotfiles/dot
+# chmod +x ~/dotfiles/dot
 
-# brewコマンドのインストール
 if [ "$(uname)" == 'Darwin' ]; then
-    #Mac
-    echo "Install brew command."
+    # Mac
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew doctor
-fi
-
-if [ "$(uname)" == 'Darwin' ]; then
-    #Mac
     brew update
     brew upgrade
     brew install reattach-to-user-namespace
@@ -22,42 +16,17 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
         sudo add-apt-repository ppa:git-core/ppa
         sudo apt -y update
         sudo apt -y upgrade
-        sudo apt -y install language-pack-ja
-        sudo apt -y install zsh
-        sudo apt -y install tmux
-        sudo apt -y install xsel
-        sudo apt -y install p7zip-full
-        sudo apt -y install build-essential
-        sudo apt -y install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev
-        sudo apt -y install python3-tk tk-dev python-tk libfreetype6-dev
+        sudo apt -y install xsel language-pack-ja build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev gettext
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+        brew install zsh tmux p7zip-full git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev python3-tk tk-dev python-tk libfreetype6-dev
         git clone https://github.com/yyuu/pyenv.git ~/.pyenv
     elif type yum > /dev/null 2>&1; then
         sudo yum update
         sudo yum upgrade
-        # git
-        sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel perl-ExtUtils-MakeMaker
-        cd /usr/local/src/
-        sudo git clone git://git.kernel.org/pub/scm/git/git.git
-        sudo yum remove -y git
-        cd git
-        sudo make prefix=/usr/local all
-        sudo make prefix=/usr/local install
-        
-        # tmux
-        cd /usr/local/src/
-        sudo yum -y install gcc libevent-devel ncurses-devel
-        sudo curl -kLO https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
-        sudo tar -zxvf tmux-2.3.tar.gz
-        cd tmux-2.3
-        sudo ./configure
-        sudo make
-        sudo make install
         sudo yum -y install xsel
-    elif type pacman > /dev/null 2>&1; then
-        sudo pacman -Sy zsh
-        sudo pacman -Sy tmux
-        sudo pacman -Sy zsel
-        sudo pacman -Sy git
+        sudo yum -y groupinstall 'Development Tools' && sudo yum -y install xsel curl git m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel openssl-devel
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+        brew install zsh tmux p7zip-full git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev python3-tk tk-dev python-tk libfreetype6-dev
     fi
 fi
 
