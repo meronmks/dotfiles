fb47cc0f6f6984d79ee3c78f30c7d33878fb62bb
meronmks <meronmks.8914@gmail.com>
2021-02-12 14:46:13 +0900

    zplugのインストール方法を調整

--- setup/install.sh
+++ setup/install.sh
@@ -28,6 +28,7 @@ if [ "$(uname)" == 'Darwin' ]; then
     brew install reattach-to-user-namespace
     brew install git
     brew install zsh
+    brew install zplug
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     export BREWHOME="/home/linuxbrew/.linuxbrew/bin"
     export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
@@ -47,7 +48,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     brew doctor
     brew update && brew upgrade
-    brew install git git-lfs zsh p7zip openssl
+    brew install git git-lfs zsh p7zip openssl zplug
     if type apt > /dev/null 2>&1; then
         $SUDO apt -y remove git
     elif type yum > /dev/null 2>&1; then
@@ -58,8 +59,6 @@ else
     exit 1
 fi
 
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-chmod -R 755 ~/.zplug
 # gitでクローンしてインストールするもの
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/takaaki-kasai/git-foresta.git ~/.git-foresta && chmod +x ~/.git-foresta
